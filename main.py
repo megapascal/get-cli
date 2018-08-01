53da11dd41d525981c7f49579e1b0d51b8c5c74b
megapascal <39701838+megapascal@users.noreply.github.com>
2018-08-01 13:54:21 +0500

    one shitty update won't change anything

--- main.py
+++ main.py
@@ -15,19 +15,14 @@ async def cleaner():
         join_hash = text_cleaner.split('/')[-1]
         print(Fore.LIGHTMAGENTA_EX + join_hash, Fore.RESET + '- your invite link hash')
         succ = await join_channel(join_hash)
-        if succ == 't':
-            channel = await client.get_entity(text_cleaner)
-            print(Fore.RESET+"Detected private channel/joined")
-            return channel
-        elif succ == 'end':
-            asyncio.get_event_loop().close()
-        elif succ == 'cont':
+        if succ in ['t', 'cont']:
             channel = await client.get_entity(text_cleaner)
             print(Fore.RESET + "Detected private channel/joined")
             return channel
+        elif succ == 'end':
+            asyncio.get_event_loop().close()
     else:
-        join_hash = text_cleaner
-        return join_hash
+        return text_cleaner
 
 
 async def main_func():

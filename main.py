f61aae68757bf703bee8c6e306536785b33b0687
Forden <mr.i.lebedev2@gmail.com>
2018-08-01 11:51:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import asyncio
 from colorama import Fore, init
-from config import get_all_members
-from config import client
-from config import join_channel
 import config
 
 init(convert=True)
@@ -14,26 +11,21 @@ async def cleaner():
     if text_cleaner.startswith('https://') or text_cleaner.startswith('t.me'):
         join_hash = text_cleaner.split('/')[-1]
         print(Fore.LIGHTMAGENTA_EX + join_hash, Fore.RESET + '- your invite link hash')
-        succ = await join_channel(join_hash)
-        if succ == 't':
+        succ = await config.join_channel(join_hash)
+        if succ in ['t', 'cont']:
             channel = await client.get_entity(text_cleaner)
             print(Fore.RESET+"Detected private channel/joined")
             return channel
         elif succ == 'end':
-            asyncio.get_event_loop().close()
-        elif succ == 'cont':
-            channel = await client.get_entity(text_cleaner)
-            print(Fore.RESET + "Detected private channel/joined")
-            return channel
+            asyncio.get_event_loop().cclose()
     else:
-        join_hash = text_cleaner
-        return join_hash
+        return text_cleaner
 
 
 async def main_func():
     if client:
         try:
-            answer = await get_all_members(await cleaner())
+            answer = await config.get_all_members(await cleaner())
             print(Fore.LIGHTGREEN_EX+'fetching(please wait...)')
             if answer:
                 print(Fore.GREEN + answer)
